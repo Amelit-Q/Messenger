299b016ae848689d380d3c09a6870e6fae822185
Amelit-Q <amelitstanding@gmail.com>
2020-10-19 12:08:11 +0300

    Updates

---
+++ precache-manifest.8a041a7c22e7bcacb7b359f5e587868f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe84b29fe4b67b75ad7f0f81ca4ce5f",
+    "url": "/Messenger/index.html"
+  },
+  {
+    "revision": "e45b2237b9d5aa23053f",
+    "url": "/Messenger/static/css/main.5331f1f5.chunk.css"
+  },
+  {
+    "revision": "7b10e1c971337ca61414",
+    "url": "/Messenger/static/js/2.df1251d6.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/Messenger/static/js/2.df1251d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45b2237b9d5aa23053f",
+    "url": "/Messenger/static/js/main.b0f3a13f.chunk.js"
+  },
+  {
+    "revision": "d8220d82c926155b8aee",
+    "url": "/Messenger/static/js/runtime-main.fddc5a5d.js"
+  }
+]);
